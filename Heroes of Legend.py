a06c1733e34ef79bf7bec3c3536a86466e8d6057
Sean Whalen <Sean Whalen>
2017-05-06 17:09:24 -0400

    Modified Table class and added Character class

--- Heroes of Legend.py
+++ Heroes of Legend.py
@@ -1,22 +1,27 @@
 #Central Casting: Heroes of Legend
 import random
 
+class Character:
+
+    def __init__(self, race):
+        self.race = race
+
 class Table:
 
-    def __init__(self, tableList = []):
+    def __init__(self, numDice, typeDice, tableList = []):
+        self.numDice = numDice
+        self.typeDice = typeDice
         self.tableList = tableList
-
-    def add(self, toAdd):
-        self.tableList.append(toAdd)
 
     def roll(self):
         """Rolls on the table"""
         if len(self.tableList) == 0:
             return None
         else:
-            return random.choice(self.tableList)
+            return tableList[rollDice(numDice, typeDice)]
 
 def rollDice(numDice, typeDice):
+    """Rolls numDice of typeDice"""
     result = 0
     for i in range(numDice):
         result += random.randint(1, typeDice)
@@ -28,8 +33,8 @@ def main():
     testTable2 = Table(exList)
     print (testTable1.roll())
     print (testTable2.roll())
-    print str(rollDice(1, 6))
-    print str(rollDice(1, 100))
+    print (rollDice(1, 6))
+    print (rollDice(1, 100))
 
 if __name__ == "__main__":
     main()
